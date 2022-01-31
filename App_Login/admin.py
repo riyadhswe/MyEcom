6a3cbd26e05e028b16950352d6e0f5d4eb2135aa
riyadhswe <riyadhswe@gmail.com>
2022-01-31 13:25:17 +0600

    Finish

--- App_Login/admin.py
+++ App_Login/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from App_Login.models import User, Profile
 
+
 # Register your models here.
+class AdminProfile(admin.ModelAdmin):
+    list_display = ['username', 'address_1', 'phone', 'date_joined']
+
 
 admin.site.register(User)
-admin.site.register(Profile)
+admin.site.register(Profile,AdminProfile)
--- App_Order/admin.py
+++ App_Order/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from App_Order.models import Cart, Order
 
-# Register your models here.
 
-admin.site.register(Cart)
-admin.site.register(Order)
+# Register your models here.
+class AdminCart(admin.ModelAdmin):
+    list_display = ['user', 'item', 'quantity', 'purchased', 'created', 'updated']
+
+
+class AdminOrder(admin.ModelAdmin):
+    list_display = ['user', 'ordered', 'created', 'orderId','paymentId']
+
+
+admin.site.register(Cart, AdminCart)
+admin.site.register(Order, AdminOrder)
