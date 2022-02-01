0b44aa3335e1413de6c0501a2ab8a84b9ecbf445
riyadhswe <riyadhswe@gmail.com>
2022-02-01 21:36:19 +0600

    Finish

--- App_Shop/views.py
+++ App_Shop/views.py
@@ -8,13 +8,15 @@ from App_Shop.models import Product
 
 # Mixin
 from django.contrib.auth.mixins import LoginRequiredMixin
-# Create your views here.
 
+
+# Create your views here.
 
 
 class Home(ListView):
     model = Product
     template_name = 'App_Shop/home.html'
+
 
 class ProductDetail(LoginRequiredMixin, DetailView):
     model = Product
